13b2ec36d1fcd3e1dd4f61047633ba3eab94b7db
se4oev <p.karpenko26@gmail.com>
2025-10-26 11:25:42 +0300

    implement multi goroutines execution for processing csv

--- rggo/performance/colStats/main.go
+++ rggo/performance/colStats/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 )
 
 func main() {
@@ -35,22 +36,45 @@ func run(filenames []string, op string, column int, out io.Writer) error {
 	}
 
 	consolidate := make([]float64, 0)
-	for _, fname := range filenames {
-		f, err := os.Open(fname)
-		if err != nil {
-			return fmt.Errorf("cannot open file: %w", err)
-		}
 
-		data, err := csv2float(f, column)
-		if err != nil {
-			return err
-		}
-		if err := f.Close(); err != nil {
-			return err
-		}
-		consolidate = append(consolidate, data...)
+	resCh := make(chan []float64)
+	errCh := make(chan error)
+	doneCh := make(chan struct{})
+	wg := sync.WaitGroup{}
+	for _, fname := range filenames {
+		wg.Add(1)
+		go func(fname string) {
+			defer wg.Done()
+			f, err := os.Open(fname)
+			if err != nil {
+				errCh <- fmt.Errorf("cannot open file: %w", err)
+				return
+			}
+			data, err := csv2float(f, column)
+			if err != nil {
+				errCh <- err
+			}
+			if err := f.Close(); err != nil {
+				errCh <- err
+			}
+			resCh <- data
+		}(fname)
 	}
 
-	_, err := fmt.Fprintln(out, opFunc(consolidate))
-	return err
+	go func() {
+		wg.Wait()
+		close(doneCh)
+	}()
+
+	for {
+		select {
+		case err := <-errCh:
+			return err
+		case data := <-resCh:
+			consolidate = append(consolidate, data...)
+		case <-doneCh:
+			_, err := fmt.Fprintln(out, opFunc(consolidate))
+			return err
+		}
+	}
 }

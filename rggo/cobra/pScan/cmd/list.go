4b2f696ff2dd376e7bed09600c0a79591d3d7311
se4oev <p.karpenko26@gmail.com>
2026-09-21 22:42:09 +0300

    add add, list, delete subcommands

---
+++ rggo/cobra/pScan/cmd/list.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+	"pragprog.com/rggo/cobra/pScan/scan"
+)
+
+var listCmd = &cobra.Command{
+	Use:     "list",
+	Aliases: []string{"l"},
+	Short:   "List hosts in hosts list",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		hostsFile, err := cmd.Flags().GetString("hosts-file")
+		if err != nil {
+			return err
+		}
+		return listAction(os.Stdout, hostsFile, args)
+	},
+}
+
+func init() {
+	hostsCmd.AddCommand(listCmd)
+}
+
+func listAction(out io.Writer, hostsFile string, args []string) error {
+	hl := &scan.HostsList{}
+	if err := hl.Load(hostsFile); err != nil {
+		return err
+	}
+
+	for _, h := range hl.Hosts {
+		if _, err := fmt.Fprintln(out, h); err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- rggo/cobra/pScan/cmd/root.go
+++ rggo/cobra/pScan/cmd/root.go
@@ -54,11 +54,11 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.pScan.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "",
+		"config file (default is $HOME/.pScan.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringP("hosts-file", "f", "pScan.hosts",
+		"pScan hosts file")
 
 	versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
 	rootCmd.SetVersionTemplate(versionTemplate)

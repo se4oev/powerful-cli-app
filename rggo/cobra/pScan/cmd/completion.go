b032834599c7b28c8ce8308e0071d8a9b509d07e
se4oev <p.karpenko26@gmail.com>
2026-09-26 21:44:14 +0300

    add docs and completion commands

---
+++ rggo/cobra/pScan/cmd/completion.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completionCmd = &cobra.Command{
+	Use:   "completion",
+	Short: "Generate bash completion for your command",
+	Long: `To load your completions run
+source <(pScan completion)
+
+To load completions automatically on login, add this line to you .bashrc file:
+$ ~/.bashrc
+source <(pScan completion)
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return completionAction(os.Stdout)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
+
+func completionAction(out io.Writer) error {
+	return rootCmd.GenBashCompletion(out)
+}

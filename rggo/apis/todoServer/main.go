31573b810aa38f8d71bdddd5ac85a136179e17e3
se4oev <p.karpenko26@gmail.com>
2026-09-26 22:14:41 +0300

    add basic rest api server

---
+++ rggo/apis/todoServer/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	host := flag.String("h", "localhost", "Server host")
+	port := flag.Int("p", 8080, "Server port")
+	todoFile := flag.String("f", "todoServer", "todo JSON file")
+	flag.Parse()
+
+	s := &http.Server{
+		Addr:         fmt.Sprintf("%s:%d", *host, *port),
+		Handler:      newMux(*todoFile),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+}
